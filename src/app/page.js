aa90e472aac7cab0b662221679129ec29bc2bb87
Grae-nz <grae-@hotmail.com>
2025-11-27 21:44:59 +1300

    Apply prefix fixes to homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import { prefix } from "../lib/prefix";
+
 export default function Home() {
   return (
     <main>
@@ -7,10 +9,17 @@ export default function Home() {
         <div className="md:w-1/2 space-y-4 md:pl-8">
           <h1 className="text-4xl font-bold" style={{ fontFamily: 'Montserrat, sans-serif' }}>The Blue Zone</h1>
           <h2 className="text-2xl" style={{ fontFamily: 'Montserrat, sans-serif' }}>Helping Kiwis better understand prostate cancer</h2>
-          <h3 className="text-base" style={{ fontFamily: 'Open Sans, sans-serif' }}>Whether you’re newly diagnosed, supporting a loved one, or just wanting to learn more, we’re here to help you every step of the way.</h3>
+          <h3 className="text-base" style={{ fontFamily: 'Open Sans, sans-serif' }}>
+            Whether you’re newly diagnosed, supporting a loved one, or just wanting to learn more, we’re here to help you every step of the way.
+          </h3>
         </div>
+
         <div className="md:w-1/2 mt-6 md:mt-0 aspect-[3/2] max-w-[700px] mx-auto">
-          <img src="/images/homeimage.png" alt="Home Page Header Image" className="w-full h-full object-cover rounded-lg"/>
+          <img
+            src={`${prefix}/images/homeimage.png`}
+            alt="Home Page Header Image"
+            className="w-full h-full object-cover rounded-lg"
+          />
         </div>
       </div>
 
@@ -43,16 +52,36 @@ export default function Home() {
         },
       ].map(({ href, title, image, description }, index) => (
         <div className="flex justify-center" key={index}>
-          <a href={href} className="transition-transform duration-300 hover:scale-105 relative w-3/4">
-            <img src={image} alt={`${title} Image`} className="object-contain rounded-lg shadow-lg w-full z-0"/>
+          <a
+            href={`${prefix}${href}`}
+            className="transition-transform duration-300 hover:scale-105 relative w-3/4"
+          >
+            <img
+              src={`${prefix}${image}`}
+              alt={`${title} Image`}
+              className="object-contain rounded-lg shadow-lg w-full z-0"
+            />
+
             <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white rounded-lg px-2 z-10">
-              <h2 className="text-2xl font-bold bg-[#00B0F0] rounded-full px-4 py-1" style={{ fontFamily: 'Montserrat, sans-serif' }}> {title}</h2>
-              <h3 className="text-sm mt-2 bg-white text-black rounded-full px-4 py-1 font-bold" style={{ fontFamily: 'Open Sans, sans-serif' }}> {description}</h3>
+              <h2
+                className="text-2xl font-bold bg-[#00B0F0] rounded-full px-4 py-1"
+                style={{ fontFamily: 'Montserrat, sans-serif' }}
+              >
+                {title}
+              </h2>
+
+              <h3
+                className="text-sm mt-2 bg-white text-black rounded-full px-4 py-1 font-bold"
+                style={{ fontFamily: 'Open Sans, sans-serif' }}
+              >
+                {description}
+              </h3>
             </div>
           </a>
         </div>
       ))}
     </div>
+
     </main>
   );
 }
